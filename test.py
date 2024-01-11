fb83866a2b28bb56b826e3976d9fc350e371ca78
mevahmarlene <156206288+mevahmarlene@users.noreply.github.com>
2024-01-11 11:19:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+def main():
+    original_string = input("Entrez la chaîne à inverser : ")
+    reversed_string = reverse_string(original_string)
+    print("Chaîne inversée :", reversed_string)
+
+if __name__ == "__main__":
+    main()
 
